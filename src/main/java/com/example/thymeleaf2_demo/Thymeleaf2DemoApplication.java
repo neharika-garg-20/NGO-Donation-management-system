76239519f054226662dba0f6549da9e59f929a67
neharika-garg-20 <neharika.garg.cse23@itbhu.ac.in>
2025-09-12 22:34:50 +0530

    initial setup

---
+++ src/main/java/com/example/thymeleaf2_demo/Thymeleaf2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf2_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleaf2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thymeleaf2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thymeleaf2_demo/Thymeleaf2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf2_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thymeleaf2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
