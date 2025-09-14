78af050b37ab3b3d6c662993713818c1240fef36
neharika-garg-20 <neharika.garg.cse23@itbhu.ac.in>
2025-09-14 12:15:30 +0530

    home controlller page changed

--- src/main/java/com/example/thymeleaf2_demo/HomeController.java
+++ src/main/java/com/example/thymeleaf2_demo/HomeController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Hello, Thymeleaf + Spring Boot!");
-        return "index"; // this looks for index.html inside templates/
-    }
-
     @GetMapping("/login")
     public String login(Model model) {
     model.addAttribute("pageTitle", "Login");
