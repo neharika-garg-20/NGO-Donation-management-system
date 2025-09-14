66185330a2b5c712edd3a81fd929ed954428ff11
AnnaNarang <70102831+AnnaNarang@users.noreply.github.com>
2025-09-14 12:25:31 +0530

    ngo directory

--- src/main/java/com/example/thymeleaf2_demo/HomeController.java
+++ src/main/java/com/example/thymeleaf2_demo/HomeController.java
@@ -3,6 +3,8 @@ package com.example.thymeleaf2_demo;  // use your actual package name
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HomeController {
@@ -12,5 +14,13 @@ public class HomeController {
     model.addAttribute("pageTitle", "Login");
     
     return "login"; 
-}
+    }
+    @GetMapping("/ngo")
+    public String ngo_listing(Model model) {
+        model.addAttribute("pageTitle", "NGO DIRECTORY");
+
+        return "ngo";
+    }
+    
+
 }
