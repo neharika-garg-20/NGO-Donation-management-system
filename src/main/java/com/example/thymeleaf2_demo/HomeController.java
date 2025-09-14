86949c7e23f98326805bc0e5442b2747f1f4be77
neharika-garg-20 <neharika.garg.cse23@itbhu.ac.in>
2025-09-14 13:20:09 +0530

    ngo profile added

--- src/main/java/com/example/thymeleaf2_demo/HomeController.java
+++ src/main/java/com/example/thymeleaf2_demo/HomeController.java
@@ -1,5 +1,4 @@
-package com.example.thymeleaf2_demo;  // use your actual package name
-
+package com.example.thymeleaf2_demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,27 +14,36 @@ public class HomeController {
     
     return "login"; 
     }
+
     @GetMapping("/ngo")
     public String ngo_listing(Model model) {
         model.addAttribute("pageTitle", "NGO DIRECTORY");
 
         return "ngo";
     }
+
     @GetMapping("/donation")
     public String donation(Model model) {
         model.addAttribute("pageTitle", "DONATION FORM");
 
         return "donation";
     }
+    
     @GetMapping("/story")
     public String story_of_impact(Model model) {
         model.addAttribute("pageTitle", "STORIES OF IMPACT");
-
         return "story_of_impact";
     }
+
     @GetMapping("/signup")
     public String signup(Model model) {
     model.addAttribute("pageTitle", "Sign Up");
     return "signup"; 
+    }
+
+    @GetMapping("/ngo_profile")
+    public String ngo_profile(Model model){
+        model.addAttribute("pageTitle" , "NGO DASHBOARD");
+        return "ngo_dashboard";
     }
 }
